d69b2a565acfbf1bfde53a4be2bbdcc6adc31c3b
ners1us <66124861+ners1us@users.noreply.github.com>
2024-02-29 14:37:40 +0300

    feat: add searching for an existing account method

--- src/main/java/Lab1/Services/SearchEngine.java
+++ src/main/java/Lab1/Services/SearchEngine.java
@@ -3,6 +3,8 @@ package Lab1.Services;
 import Lab1.Entities.Account.Account;
 import Lab1.Entities.Bank.Bank;
 import Lab1.Models.Client.Client;
+import Lab1.Models.ValueObjects.Name;
+import Lab1.Models.ValueObjects.Surname;
 
 public class SearchEngine {
     public static Client findClient(String firstName, String lastName, Bank bank) {
@@ -13,7 +15,14 @@ public class SearchEngine {
         }
         return null;
     }
-
+    public static Account findAccount(Client client, Bank bank) {
+        for (Account account : bank.getAccounts()) {
+            if (client.getFirstName().getValue().equals(account.getClient().getFirstName().getValue()) && client.getSurName().getValue().equals(account.getClient().getSurName().getValue())) {
+                return account;
+            }
+        }
+        return null;
+    }
     public static boolean checkIfClientExists(Client client, Bank bank) {
         return bank.getClients().contains(client);
     }
